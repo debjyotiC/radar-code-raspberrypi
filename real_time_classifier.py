7b83a84de66312e2a0405bd0d465e0b8ee60f1b2
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2023-07-14 19:20:46 +0530

    detected array

--- real_time_classifier.py
+++ real_time_classifier.py
@@ -120,7 +120,7 @@ def classifier_func(rangeArray, range_doppler):
     time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     if predicted_class == "Human_Present":
-        db = {'Prediction': predicted_class, "Score": std_peaks, "Detected objects": picked_elements, 'Time': time_now}
+        db = {'Prediction': predicted_class, "Score": std_peaks, "Detected objects": picked_elements.tolist(), 'Time': time_now}
     else:
         db = {'Prediction': predicted_class, "Score": std_peaks, "Detected objects": [], 'Time': time_now}
 
